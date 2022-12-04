39247f67db1c53a01cb4294dd79760f3450cdb18
Isa <i.agverdiev@mastercr.ru>
2022-12-04 19:43:01 +0300

    read

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,10 @@
-const read = async () => {
-    // Write your code here 
-};
+import * as fs from 'fs'
 
-await read();
+const read = async () => {
+	fs.readFile('src/fs/files/fileToRead.txt', 'utf-8', (err, data) => {
+		if (data) console.log(data)
+		if (err) console.log('FS operation failed')
+	})
+}
+
+await read()
