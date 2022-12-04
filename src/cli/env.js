199f8eff8c82a739441f3483e7dcec3feb8778a5
Isa <i.agverdiev@mastercr.ru>
2022-12-04 23:23:46 +0300

    cli:env

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,9 @@
 const parseEnv = () => {
-    // Write your code here 
+    for (let env of Object.entries(process.env)) {
+        if (env[0].includes('RSS_')) {
+            console.log(`${env[0]} = ${env[1]}`);
+        }
+    }
 };
 
 parseEnv();
