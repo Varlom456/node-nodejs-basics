fe1dda088c526e72e558f538c56d377ffa6dd234
Isa <i.agverdiev@mastercr.ru>
2022-12-04 23:11:45 +0300

    copy

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,32 @@
+import * as fs from 'fs'
+
+const dir = 'src/fs/files'
+const dirCopy = 'src/fs/files_copy'
+
+
 const copy = async () => {
-    // Write your code here 
+// проверяем наличие папки
+    fs.access(dirCopy, (err) => {
+        if (err) {
+            //    создаем дирикторию для файлов
+            fs.mkdir(dirCopy, {recursive: true}, (err) => {
+            });
+        } else {
+            console.log("FS operation failed");
+        }
+// находим имена файлов в папке files
+        fs.readdir(dir, (err, data) => {
+            if (err) console.log("FS operation failed");
+            data.forEach(file => {
+                // добалвяем файл в созданную папку files_copy
+                fs.copyFile(`${dir}/${file}`, `${dirCopy}/${file}`, err => {
+                    if (err) console.log('FS operation failed')
+                })
+            });
+        });
+    });
+
 };
 
 await copy();
+
