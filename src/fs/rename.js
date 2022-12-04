b75fd0cc91eee961e958ce19d2e73a02d09d8bdb
Isa <i.agverdiev@mastercr.ru>
2022-12-04 19:43:15 +0300

    rename

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,13 @@
-const rename = async () => {
-    // Write your code here 
-};
+import * as fs from 'fs'
 
-await rename();
+const rename = async () => {
+	fs.rename(
+		'src/fs/files/wrongFilename.txt',
+		'src/fs/files/properFilename.txt',
+		err => {
+			if (err) console.log('FS operation failed')
+		}
+	)
+}
+
+await rename()
